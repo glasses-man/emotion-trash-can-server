917a487e095156b4f292989fbd96ad47526d12b2
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-06-21 11:17:30 +0900

    기본 세팅

---
+++ src/test/java/com/emotion/trash/can/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emotion.trash.can.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
