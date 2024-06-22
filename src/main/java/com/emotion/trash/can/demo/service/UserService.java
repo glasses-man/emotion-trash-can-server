24def923a99db1733fff05003a8476fcaa44b75b
dreamer603 <mj617066@gmail.com>
2024-06-22 19:00:28 +0900

    UserService Interface 완성

---
+++ src/main/java/com/emotion/trash/can/demo/service/UserService.java
@@ -0,0 +1,33 @@
+package com.emotion.trash.can.demo.service;
+
+import com.emotion.trash.can.demo.dto.UserDTO;
+import com.emotion.trash.can.demo.entity.UserEntity;
+
+public interface UserService {
+    Long register(UserDTO dto);
+    UserDTO get(Long userID);
+
+    void modify(UserDTO dto);
+    void remove(Long userID);
+
+    default UserEntity dtoToEntity(UserDTO dto) {
+        UserEntity entity = UserEntity.builder()
+                .userID(dto.getUserID())
+                .id(dto.getId())
+                .password(dto.getPassword())
+                .userName(dto.getUserName())
+                .build();
+        return entity;
+    }
+
+    default UserDTO entityToDto(UserEntity entity) {
+        UserDTO dto = UserDTO.builder()
+                .userID(entity.getUserID())
+                .id(entity.getId())
+                .password(entity.getPassword())
+                .userName(entity.getUserName())
+                .build();
+        return dto;
+    }
+
+}
